0e29e3eb7d6f07ce4e11a706e8d99e303f9315c7
りょういち <82487260+ryoichi1017@users.noreply.github.com>
2021-04-15 18:52:12 +0900

    Hunger
    練習です

---
+++ Hunger.java
@@ -0,0 +1,16 @@
+public class Hunger {
+  public static void main(String[] args) {
+    int isHungry = 0;
+        String food = "肉";
+        System.out.println("こんにちは");
+        if (isHungry == 0) {
+          System.out.println("お腹がいっぱいです");
+        }else {
+          System.out.println("はらぺこです");
+        }
+        if (isHungry == 1) {
+          System.out.println(food + "をいただきます");
+       }
+    System.out.println("ごちそうさまでした");
+  }
+}
